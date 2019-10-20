a8d4fc7fb96a0702029d30c47b827f4949b33c3e
Alex Khoa Le <leduykhoa@hotmail.ca>
2019-10-19 21:49:28 -0400

    The carousel now reappears when a player Pokemon faints or the player clicks the Switch button

--- assets/scripts/battleSys.js
+++ assets/scripts/battleSys.js
@@ -19,14 +19,12 @@ function attackOpponent(attacker, defender) {
     }
 
     defender.hpCurrent -= damage;
-    console.log(`${defender.name} took ${damage} damage! HP: ${defender.hpCurrent}/${defender.hp}`);
     if (defender.hpCurrent <= 0) {
         defender.hpCurrent = 0;
         defender.status = "fainted"
         console.log(`${defender.name} took ${damage} damage! It has ${defender.status}!`);
         if (defender == activeComputerPokemon) {
             numActiveComputerPokemon--;
-            console.log(numActiveComputerPokemon);
             if (numActiveComputerPokemon > 0) {
                 switchCompActivePokemon(computerTeam, Math.floor(Math.random() * 6), activeComputerPokemon);
             } else {
@@ -34,32 +32,32 @@ function attackOpponent(attacker, defender) {
             }
         } else {
             numActivePlayerPokemon--;
-            if (numActivePlayerPokemon <= 0) {
-                console.log(`You have no active Pokemon left! You Lose!`);
-            } else {
-                console.log(`Please switch to your next Pokemon`);
-            }
+            $(".carousel > .active").addClass("pkmnFainted");
+            refreshCarousel();
         }
     }
 
     if (defender == activeComputerPokemon) {
         $("#compHP").text(`${defender.hpCurrent}/${defender.hp}`);
-        $("#compHPbar").css("width", `${Math.round(defender.hpCurrent/defender.hp*100)}%`)
+        $("#compHPbar").css("width", `${Math.round(defender.hpCurrent / defender.hp * 100)}%`)
         attackOpponent(defender, attacker);
     } else if (defender == activePlayerPokemon) {
         $("#playerHP").text(`${defender.hpCurrent}/${defender.hp}`);
-        $("#playerHPbar").css("width", `${Math.round(defender.hpCurrent/defender.hp*100)}%`)
+        $("#playerHPbar").css("width", `${Math.round(defender.hpCurrent / defender.hp * 100)}%`)
     }
 }
 
 $('#pokemonSelectBtn').click(function () {
+    if (activePlayerPokemon.status == "fainted") {
+        return;
+    }
     $("#teamSelection").removeClass("fadeIn").addClass("slideOut");
     setActivePlayerPokemonInfo(activePlayerPokemon);
     setActiveComputerPokemonInfo(activeComputerPokemon);
 
     setTimeout(function () {
-        $("#teamSelection").css("display", "none");
-        $("#battleContainer").css("display", "block");
+        $("#teamSelection").addClass("hide");
+        $("#battleContainer").removeClass("hide");
         $("#playerSpriteImg").addClass("slideInFromLeft");
         $("#compSpriteImg").addClass("slideInFromRight");
     }, 700)
@@ -82,12 +80,13 @@ $('#playerCarousel').mousedown(function () {
 })
 
 function switchCompActivePokemon(team, pokemonIdx, activePokemon) {
+    if (numActiveComputerPokemon == 1) {
+        activeComputerPokemon = computerTeam.find(obj => obj.status === "ready");
+        setActiveComputerPokemonInfo(activeComputerPokemon);
+        return;
+    }
     if (team[pokemonIdx].status != "fainted") {
-        if (numActiveComputerPokemon == 1){
-            activeComputerPokemon = computerTeam.find(obj => obj.status === "ready");
-        } else {
-            activeComputerPokemon = team[pokemonIdx];
-        }
+        activeComputerPokemon = team[pokemonIdx];
         console.log(`${activeComputerPokemon.name} was switched out!`)
         setActiveComputerPokemonInfo(activeComputerPokemon);
     } else {
@@ -99,11 +98,12 @@ function switchCompActivePokemon(team, pokemonIdx, activePokemon) {
 function switchPlayerActivePokemon(pokemonIdx) {
     if (numActivePlayerPokemon <= 0) {
         console.log(`You don't have any usable Pokemon!`)
-    } else if (playerTeam[pokemonIdx].status != "fainted") {
+        // } else if (playerTeam[pokemonIdx].status != "fainted") {
+    } else {
         activePlayerPokemon = playerTeam[pokemonIdx];
         setActivePlayerPokemonInfo(activePlayerPokemon);
-    } else {
-        console.log(`Cannot switch! This Pokemon has fainted!`)
+        // } else {
+        //     console.log(`Cannot switch! This Pokemon has fainted!`)
     }
 }
 
@@ -111,24 +111,32 @@ function setActivePlayerPokemonInfo(pokemonInfo) {
     $("#playerPkmnName").text(pokemonInfo.name);
     $("#playerHP").text(`${pokemonInfo.hpCurrent}/${pokemonInfo.hp}`);
     $("#playerSpriteImg").attr("src", pokemonInfo.spriteBack);
-    $("#playerHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent/pokemonInfo.hp*100)}%`)
+    $("#playerHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent / pokemonInfo.hp * 100)}%`)
 }
 
 function setActiveComputerPokemonInfo(pokemonInfo) {
     $("#compPkmnName").text(pokemonInfo.name);
     $("#compHP").text(`${pokemonInfo.hpCurrent}/${pokemonInfo.hp}`);
     $("#compSpriteImg").attr("src", pokemonInfo.spriteFront);
-    $("#compHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent/pokemonInfo.hp*100)}%`)
+    $("#compHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent / pokemonInfo.hp * 100)}%`)
+}
+
+function refreshCarousel() {
+    $("#teamSelection").removeClass("hide slideOut").addClass("fadeIn");
+    $("#battleContainer").addClass("hide");
+    $('.card-content').html(`
+    <span class="card-title">${activePlayerPokemon.name}</span>
+    <p>HP: ${activePlayerPokemon.hpCurrent}/${activePlayerPokemon.hp}</p>
+    <p>ATK: ${activePlayerPokemon.attack}</p>
+    <p>DEF: ${activePlayerPokemon.defense}</p>
+    `);
 }
 
 $('#attackBtn').on('click', function () {
-    console.log("button clicked");
     attackOpponent(activePlayerPokemon, activeComputerPokemon);
 
 })
 
 $('#switchBtn').on('click', function () {
-    console.log("button clicked");
-
-
+    refreshCarousel();
 })
