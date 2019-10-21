81b886ffa334fc4b5ce129f0bdcfc27743a97ace
Paul B-H <51964537+PaulB-H@users.noreply.github.com>
2019-10-21 00:36:13 -0400

    Matching branch Chris

--- assets/scripts/battleSys.js
+++ assets/scripts/battleSys.js
@@ -1,6 +1,11 @@
 var critMultiplier = 0.25;
 var allowPlayerInput = true;
+var audioAttack = document.createElement("audio");
+audioAttack.setAttribute("src", 'assets/musictones/Barrage_1hit.mp3');
+var audioBattle = document.createElement("audio");
+audioBattle.setAttribute("src", "assets/musictones/115-battlevstrainer.mp3");
 
+// Primary attack function. Takes the attacking Pokemon, defending Pokemon, and the attack type
 function attackOpponent(attacker, defender, type) {
     if ((attacker.status == "fainted") || (defender.status == "fainted")) {
         console.log(`This Pokemon is unable to continue battling! Please switch to another Pokemon!`);
@@ -10,7 +15,7 @@ function attackOpponent(attacker, defender, type) {
     // Checks if the attack is a critical hit (6.25% chance)
     let isCrit = (0.0625 > Math.random());
     let damage = 0;
-    // Total damage has a +/- 15% variability
+    // Total damage has a +/- 15% variability by making total damage random from 85% to 115%
     if (type == "normal") {
         damage = Math.round((attacker.attack + (attacker.attack * critMultiplier * isCrit) - defender.defense) * ((Math.random() * 0.30) + 0.85));
     } else {
@@ -22,6 +27,8 @@ function attackOpponent(attacker, defender, type) {
     }
 
     defender.hpCurrent -= damage;
+
+    // If the defending Pokemon faints, switch to a new Pokemon if available
     if (defender.hpCurrent <= 0) {
         defender.hpCurrent = 0;
         defender.status = "fainted"
@@ -56,11 +63,13 @@ function attackOpponent(attacker, defender, type) {
             $('#runBtn').removeClass('disabled');
         }
     }
-
+    
+    // If the defending Pokemon is the computer's randomly choose a normal or special attack and counter
     if (defender == activeComputerPokemon) {
         damageToaster(defender.name, damage);
         let chooseAttack = Math.round(Math.random());
         let attackType = "normal";
+        
         if (chooseAttack) {
             attackType = "special";
         }
@@ -70,9 +79,10 @@ function attackOpponent(attacker, defender, type) {
         setTimeout(function () {
             $('#playerSpriteImg').removeClass('playerAttack');
             $('#compSpriteImg').addClass('computerAttack');
-            console.log(attackType);
             attackOpponent(defender, attacker, attackType);
         }, 1000)
+
+    // Otherwise if the defender is the player's Pokemon, reenable input after taking damage
     } else if (defender == activePlayerPokemon) {
         damageToaster(defender.name, damage);
 
@@ -89,6 +99,7 @@ function attackOpponent(attacker, defender, type) {
     }
 }
 
+// Toasters
 function damageToaster(target, damage) {
     toastr.info(`${target} took ${damage} damage!`);
     toastr.options = {
@@ -149,6 +160,7 @@ function faintedToaster() {
     };
 }
 
+// Switch the computer's Pokemon if their active Pokemon faints, but only if the one chosen is usable
 function switchCompActivePokemon(team, pokemonIdx, activePokemon) {
     if (numActiveComputerPokemon == 1) {
         activeComputerPokemon = computerTeam.find(obj => obj.status === "ready");
@@ -165,6 +177,7 @@ function switchCompActivePokemon(team, pokemonIdx, activePokemon) {
     }
 }
 
+// Switch the active Pokemon to the player's selection
 function switchPlayerActivePokemon(pokemonIdx) {
     if (numActivePlayerPokemon <= 0) {
         console.log(`You don't have any usable Pokemon!`)
@@ -174,6 +187,7 @@ function switchPlayerActivePokemon(pokemonIdx) {
     }
 }
 
+// Set the active player's Pokemon info, even if it's unusable
 function setActivePlayerPokemonInfo(pokemonInfo) {
     $("#playerPkmnName").text(pokemonInfo.name);
     $("#playerHP").text(`${pokemonInfo.hpCurrent}/${pokemonInfo.hp}`);
@@ -181,6 +195,7 @@ function setActivePlayerPokemonInfo(pokemonInfo) {
     $("#playerHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent / pokemonInfo.hp * 100)}%`)
 }
 
+// Set the active computer's Pokemon info
 function setActiveComputerPokemonInfo(pokemonInfo) {
     $("#compPkmnName").text(pokemonInfo.name);
     $("#compHP").text(`${pokemonInfo.hpCurrent}/${pokemonInfo.hp}`);
@@ -188,6 +203,7 @@ function setActiveComputerPokemonInfo(pokemonInfo) {
     $("#compHPbar").css("width", `${Math.round(pokemonInfo.hpCurrent / pokemonInfo.hp * 100)}%`)
 }
 
+// Show the carousel again when called in battle
 function refreshCarousel() {
     $("#teamSelection").removeClass("hide slideOut").addClass("fadeIn");
     $("#battleContainer").addClass("hide");
@@ -201,6 +217,7 @@ function refreshCarousel() {
     `);
 }
 
+// Wait for a delay, then call the update
 function updateCurrentCarouselInfo(delay) {
     setTimeout(function () {
         let currentCarousel = $(".carousel > .active");
@@ -217,7 +234,7 @@ function updateCurrentCarouselInfo(delay) {
     }, delay)
 }
 
-// set ActivePlayerPokemon to be equal to carousel-item active
+// set ActivePlayerPokemon to be equal to carousel-item active when the mouse is held down or when mouse is released
 $('#playerCarousel').mousedown(function () {
     updateCurrentCarouselInfo(1000);
 })
@@ -226,14 +243,18 @@ $('#playerCarousel').mouseup(function () {
     updateCurrentCarouselInfo(600);
 })
 
+// Select the Pokemon for battle if they're usable
 $('#pokemonSelectBtn').click(function () {
+    
     if (activePlayerPokemon.status == "fainted") {
         faintedToaster();
         return;
     }
+
     $("#teamSelection").removeClass("fadeIn").addClass("slideOut");
     setActivePlayerPokemonInfo(activePlayerPokemon);
     setActiveComputerPokemonInfo(activeComputerPokemon);
+    audioStart.pause();
 
     setTimeout(function () {
         $("#teamSelection").addClass("hide");
@@ -241,14 +262,15 @@ $('#pokemonSelectBtn').click(function () {
         $("#playerSpriteImg").addClass("slideInFromLeft");
         $("#compSpriteImg").addClass("slideInFromRight");
         setTimeout(function () {
+            audioBattle.play();
             $("#playerSpriteImg").removeClass("slideInFromLeft");
             $("#compSpriteImg").removeClass("slideInFromRight");
         }, 700)
     }, 700)
-
 })
 
 $('#attackBtn').on('click', function () {
+
     if (allowPlayerInput) {
         allowPlayerInput = false;
         $('#attackBtn').addClass('disabled');
@@ -257,6 +279,7 @@ $('#attackBtn').on('click', function () {
         $('#runBtn').addClass('disabled');
         $('#playerSpriteImg').addClass('playerAttack');
         attackOpponent(activePlayerPokemon, activeComputerPokemon, "normal");
+        audioAttack.play();
     }
 })
 
